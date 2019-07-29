4c1492e173699c0489191cd507558320d6a93c61
anfer86 <anfer86@gmail.com>
2019-07-29 10:10:50 -0300

    primeira versão

---
+++ readme.md
@@ -0,0 +1,32 @@
+# Projeto Consulta CEP com Javascript
+
+Este é um projeto para ensino de Javascript. O objetivo do projeto é aprender a criar e atribuir funções em Javasript, bem como realizar requisições assíncronas usando a classe `XMLHttpRequest`. No projeto usamos essas ferramentas para realizara consulta de CEP (CÓDIGO DE ENDEREÇAMENTO POSTAL) do Brasil.
+
+## Pré-requisitos
+
+* Cliente Git (https://git-scm.com). Verifique a instalação digitando:
+```bash
+$ git --version
+```
+
+## Instalação
+
+Clonar o repositório do GitHub e usar o `npm` do Node.js para instalar as dependências do projeto.
+```bash
+$ git clone https://github.com/anfer86/teaching-js-consulta-cep.git
+$ cd teaching-js-consulta-cep
+```
+
+## Utilização
+
+Para rodar a aplicação abrir o arquivo `src/formulario_cep.html`:
+
+## Desenvolvimento
+
+O projeto foi desenvolvido com:
+
+* [Sublime Text](https://www.sublimetext.com/) - Editor de texto para programação
+
+## Autores
+
+* **Carlos Andres Ferrero** - [anfer86](https://github.com/anfer86)
---
+++ src/helper_pesquisacep.js
@@ -0,0 +1,106 @@
+﻿/**
+ * Esta função limpa os campos de rua, bairro, cidade e uf
+ * 
+ * @param {string} id_rua       id do elemento HTML para mostrar a rua
+ * @param {string} id_bairro    id do elemento HTML para mostrar o bairro 
+ * @param {string} id_cidade    id do elemento HTML para mostrar a cidade
+ * @param {string} id_estado    id do elemento HTML para mostrar o estado 
+ */
+function limparDadosCep(id_rua, id_bairro, id_cidade, id_estado) {
+    document.getElementById(id_rua).value    = "";
+    document.getElementById(id_bairro).value = "";
+    document.getElementById(id_cidade).value = "";
+    document.getElementById(id_estado).value = "";
+}
+
+/**
+ * Esta função mostra a mensagem de erro em um elemento HTML
+ * 
+ * @param {string} mensagem mensagem de erro para mostrar
+ * @param {string} id_msg_erro id do elemento para aparecer a mensagem
+ */
+function mostrarMsgErro(mensagem, id_msg_erro){
+    document.getElementById(id_msg_erro).innerHTML = mensagem;    
+}
+
+/**
+ * Esta função apaga a mensagem de erro em um elemento HTML
+ * 
+ * @param {string} id_msg_erro id do elemento com a mensagem de erro
+ */
+function apagarMsgErro(id_msg_erro){
+    document.getElementById(id_msg_erro).innerHTML = "";
+}
+
+/**
+ * Esta função verifica se o formato do cep é válido. Um formato de cep 
+ * válido consistem em 8 caracteres com apenas números.
+ *  
+ * @param {string} cep  Número do CEP como cadeia de caracteres.
+ * @returns {boolean}
+ */
+function isFormatoCepValido(cep){
+    var regex_cepvalido = /^[0-9]{8}$/;
+    return regex_cepvalido.test(cep);    
+}
+
+function processarCepValido(dados_cep, id_rua, id_bairro, id_cidade, id_estado){    
+    document.getElementById(id_rua).value    = dados_cep.logradouro;
+    document.getElementById(id_bairro).value = dados_cep.bairro;
+    document.getElementById(id_cidade).value = dados_cep.localidade;
+    document.getElementById(id_estado).value = dados_cep.uf;    
+}
+
+/**
+ * Esta função é responsável por procurar um CEP e preencher os campos
+ * O algoritmo consiste nos seguintes passos
+ * 
+ * 1. Armazenar o valor do CEP em uma variável
+ * 2. Verificar se o formato do CEP é inválido. 
+ *  1. Se for mostrar mensagem de erro e retorna
+ *  2. Senão continua a execução da função
+ * 3. Criar uma requisição HTTP
+ *  1. Se a requisição for bem sucedida armazenar o resultado em uma variável
+ *  2. Verificar se o resultado contém a string 'erro' (caso CEP inexistente)
+ *   1. Se tiver erro mostrar a mensagem de 'CEP Inexistente'
+ *   2. Senão processa o CEP válido
+ * 4. Executar a requisição 
+ * 
+ * @param {string} id_cep       id do elemento HTML que contém o cep
+ * @param {string} id_rua       id do elemento HTML para mostrar a rua
+ * @param {string} id_bairro    id do elemento HTML para mostrar o bairro 
+ * @param {string} id_cidade    id do elemento HTML para mostrar a cidade
+ * @param {string} id_estado    id do elemento HTML para mostrar o estado
+ * @param {string} id_msg_erro  id do elemento HTML para mostrar mensagens de erro 
+ */
+function pesquisarCep(id_cep, id_rua, id_bairro, id_cidade, id_estado, id_msg_erro){
+	
+    var cep = document.getElementById(id_cep).value;    
+    
+    if (!isFormatoCepValido(cep)){
+        mostrarMsgErro("Formato de CEP inválido", id_msg_erro);
+        limparDadosCep(id_rua, id_bairro, id_cidade, id_estado);    
+        return;
+    }
+    
+    var xhr = new XMLHttpRequest();
+    
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
+            
+            var jsonObject = JSON.parse(xhr.responseText);
+
+            if ( 'erro' in jsonObject){
+                mostrarMsgErro("CEP Inexistente", id_msg_erro);
+                limparDadosCep(id_rua, id_bairro, id_cidade, id_estado);            
+            } else {
+                apagarMsgErro(id_msg_erro);
+                processarCepValido(jsonObject, id_rua, id_bairro, id_cidade, id_estado);
+            }
+        } 
+    }
+
+    url = 'https://viacep.com.br/ws/'+ cep + '/json/';      
+    xhr.open('GET', url);
+    xhr.send();
+}
